1b6db9c6d3fa712ad3de549a399ff8339f68b0e0
Tim Smith <tsmith84@gmail.com>
2015-09-09 22:19:58 -0700

    Add supermarket metadata

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Scott Hain'
 maintainer_email 'shain@chef.io'
 license 'Apache 2.0'
 description 'Installs/Configures kimchi'
-long_description 'Installs/Configures kimchi'
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.0'
 
 supports 'centos'
@@ -11,3 +11,6 @@ supports 'ubuntu'
 
 depends 'runit', '~> 1.6'
 depends 'iptables', '~> 0.13.2'
+
+source_url 'https://github.com/chef-cookbooks/kimchi' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/kimchi/issues' if respond_to?(:issues_url)
