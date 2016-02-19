c22bcd211efc7a3f12e4c636224402b23d61c7d1
Tim Smith <tsmith@chef.io>
2016-02-18 16:23:56 -0800

    Remove iptables version constraint

--- metadata.rb
+++ metadata.rb
@@ -10,7 +10,7 @@ supports 'centos'
 supports 'ubuntu'
 
 depends 'runit', '~> 1.6'
-depends 'iptables', '~> 0.13.2'
+depends 'iptables'
 
 source_url 'https://github.com/chef-cookbooks/kimchi' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-cookbooks/kimchi/issues' if respond_to?(:issues_url)
